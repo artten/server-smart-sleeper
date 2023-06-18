ca86168ff4fc85fcd0bbb0478ec37f6452a8774d
SaarAloni <alonisaar@gmail.com>
2023-06-18 11:23:40 +0300

    add constant and improved the sim score for hours similarity

--- SRAI.py
+++ SRAI.py
@@ -9,6 +9,7 @@ from sklearn.cluster import KMeans
 from collections import Counter
 import Util
 
+
 def get_sleep_of_user_from_db(email):
     mydb = Util.connect_to_db()
 
@@ -42,7 +43,7 @@ def two_hours_close(h1, h2, time):
     h2 = int(h2)
     if (max(h1, h2) - min(h1, h2)) < time:
         return True
-    if (min(h1, h2) + 1440) - max(h1, h2) < time:
+    if (min(h1, h2) + Util.num_min_in_day) - max(h1, h2) < time:
         return True
     return False
 
@@ -51,7 +52,7 @@ def dist_between_hours(h1, h2):
     h1 = int(h1)
     h2 = int(h2)
     dist = max(h1, h2) - min(h1, h2)
-    dist2 = (min(h1, h2) + 1440) - max(h1, h2)
+    dist2 = (min(h1, h2) + Util.num_min_in_day) - max(h1, h2)
     return min(dist2, dist)
 
 
@@ -70,63 +71,59 @@ def mass_center(data):
 # given sleep start time of user recommend time for waking up receive the user sleep records, for the sake of
 # convenience the format for a record is: start time, end time , rating
 def recommend_wake_time(start_time, records):
-    #mask = two_hours_close(records[:, 3], start_time, 120)
-    #print(mask)
     relevant = []
 
     for i in range(len(records)):
         print(records[i][4])
-        if two_hours_close(records[i][3], start_time, 120):
+        if two_hours_close(records[i][3], start_time, Util.min_in_2_hours):
             relevant.append(records[i])
 
-    #relevant = (records[mask])[0]
     relevant = np.array(relevant)
     print(relevant)
     if len(relevant) == 0:
-        return start_time + 480
-    return ((np.array(relevant[:, 4]).astype(float) * np.array(relevant[:, 2]).astype(float)).sum()) / (np.array(relevant[:, 2]).astype(float).sum())
+        return start_time + Util.min_in_8_hours
+    return ((np.array(relevant[:, 4]).astype(float) * np.array(relevant[:, 2]).astype(float)).sum()) / (
+        np.array(relevant[:, 2]).astype(float).sum())
 
 
 # given sleep end time of user recommend time for starting to sleep, receive the user sleep records, for the sake of
 # convenience the format for a record is: start time, end time , rating
 def recommend_sleep_time(wake_time, records):
-    #mask = two_hours_close(records[:, 3], start_time, 120)
-    #print(mask)
     relevant = []
     for i in range(len(records)):
-        if two_hours_close(records[i][4], wake_time, 120):
+        if two_hours_close(records[i][4], wake_time, Util.min_in_2_hours):
             relevant.append(records[i])
 
-    #relevant = (records[mask])[0]
     relevant = np.array(relevant)
     if len(relevant) == 0:
-        time = wake_time - 480
+        time = wake_time - Util.min_in_8_hours
         if time < 0:
-            time += 1440
+            time += Util.num_min_in_day
         return time
-    return ((np.array(relevant[:, 3]).astype(float) * np.array(relevant[:, 2]).astype(float)).sum()) / (np.array(relevant[:, 2]).astype(float).sum())
+    return ((np.array(relevant[:, 3]).astype(float) * np.array(relevant[:, 2]).astype(float)).sum()) / (
+        np.array(relevant[:, 2]).astype(float).sum())
 
 
+# given 2 array of hours, check how much the second array can contribute in similarity to the first array
 # this similarity is not symmetric
 def hours_array_sim(hours1, hours2):
     div = len(hours1)
     sim_score = 0
     for k1, val1 in hours1.items():
+        # limit how much one hour can contribute to the sim score
+        limit = Util.per_hour_sim_factor / div
         sum_val = 0
         for k2, val2 in hours2.items():
-            if two_hours_close(k1, k2, 60):
-                sum_val += (1 - (dist_between_hours(k1, k2) / 60)) * (min((val2 / val1), 1))
-        sim_score += min(1.0, sum_val / div)
+            if two_hours_close(k1, k2, Util.min_in_1_hour):
+                sum_val += (1 - (dist_between_hours(k1, k2) / Util.min_in_1_hour)) * (min((val2 / val1), 1))
+        sim_score += min(1.0, sum_val / div, limit)
     return min(sim_score, 1.0)
 
 
 class Recommender:
-    def __init__(self, strategy='user'):
-        self.strategy = strategy
-        self.content_sim = np.NaN
+    def __init__(self):
         self.similarity = np.NaN
-        self.matrix = np.NaN
-        self.recNum = 2
+        self.recNum = 3
         self.users_list = np.NaN
 
     def organize_user(self, users):
@@ -137,14 +134,14 @@ class Recommender:
         arr = []
 
         for email in self.users_list:
-            #arr[i] = sleeps[sleeps[:, 1] == email][:, 3]
+            # arr[i] = sleeps[sleeps[:, 1] == email][:, 3]
             hours = np.array(sleeps[sleeps[:, 1] == email][:, 3]).astype(int)
 
             counter = Counter(hours)
             arr.append(counter)
-            #if len(hours) < 10:
+            # if len(hours) < 10:
             #    arr.append({val: 1 for val in hours})
-            #else:
+            # else:
             #    arr.append(mass_center(hours))
 
         arr = np.array(arr, dtype=object)
@@ -154,43 +151,38 @@ class Recommender:
         arr = []
 
         for email in self.users_list:
-            #arr[i] = sleeps[sleeps[:, 1] == email][:, 3]
+            # arr[i] = sleeps[sleeps[:, 1] == email][:, 3]
             hours = np.array(sleeps[sleeps[:, 1] == email][:, 4]).astype(int)
             counter = Counter(hours)
             arr.append(counter)
-            #if len(hours) < 10:
+            # if len(hours) < 10:
             #    arr.append({val: 1 for val in hours})
-            #else:
+            # else:
             #    arr.append(mass_center(hours))
-
 
         arr = np.array(arr, dtype=object)
         return arr
 
     # receive data of 2 users and calc similarity
     def content_sim_between_2users(self, a, b):
-        weights = np.array([0.25, 0.25, 0.25, 0.25])
-        diff_arr = np.array([])
-        years = 1825
         similarity = 0
         # age is similar if at the very least they are 5 years apart
-        if abs(a[0] - b[0]) < years:
-            similarity += (1 - (abs((a[0] - b[0]) / 1825))) * weights[0]
+        if abs(a[0] - b[0]) < Util.days_in_5_years:
+            similarity += (1 - (abs((a[0] - b[0]) / Util.days_in_5_years))) * Util.content_sim_weight
         # is same gender
         if a[1] == b[1]:
-            similarity += weights[1]
+            similarity += Util.content_sim_weight
         # height is similar if at the very least they are 20 cem apart
-        if abs(a[2] - b[2]) < 0.2:
-            similarity += (1 - (abs((a[2] - b[2]) / 0.2))) * weights[2]
+        if abs(a[2] - b[2]) < Util.height_sim:
+            similarity += (1 - (abs((a[2] - b[2]) / Util.height_sim))) * Util.content_sim_weight
         # weight is similar if at the very least they are 15 kilos apart
-        if abs(a[3] - b[3]) < 15:
-            similarity += (1 - (abs((a[3] - b[3]) / 15))) * weights[3]
+        if abs(a[3] - b[3]) < Util.weight_sim:
+            similarity += (1 - (abs((a[3] - b[3]) / Util.weight_sim))) * Util.content_sim_weight
         return similarity
 
     # this function receive a matrix containing all the users data, and a function that receive info of 2 people and
     # return similarity between them
     def calc_similarity(self, matrix, sim_func):
-        self.matrix = matrix
         # a = matrix.shape
         a = len(self.users_list)
         result = np.zeros((a, a))
@@ -199,12 +191,13 @@ class Recommender:
 
         for i in range(a):
             for j in range(a):
-
                 result[i][j] = sim_func(matrix[i], matrix[j])
-        #for i in range(a):
-         #   for j in range(i + 1, a):
-          #      result[i][j] = sim_func(matrix[i], matrix[j])
-           #     result[j][i] = result[i][j]
+        for i in range(a):
+            result[i][i] = 1
+        # for i in range(a):
+        #   for j in range(i + 1, a):
+        #      result[i][j] = sim_func(matrix[i], matrix[j])
+        #     result[j][i] = result[i][j]
         return result
 
     # this function receive a matrix of records of different id's and sim func return similarity
@@ -213,7 +206,6 @@ class Recommender:
     # this function receive all the needed database data on all users and create a similarity matrix
     def train(self, users, sleep_records):
         # each factor is given different weight in the similarity matrix
-        weights = np.array([1, 3])
         # first organize the data into structure that can be sent to
         content_mat = self.organize_user(users)
         # sleep has many factors that can be considered, start time, end time, and how long the sleep was,
@@ -221,14 +213,11 @@ class Recommender:
         sleep_mat_start = self.organize_sleep_start(sleep_records)
         sleep_mat_end = self.organize_sleep_end(sleep_records)
 
-        self.content_sim = self.calc_similarity(content_mat, self.content_sim_between_2users)
+        content_sim = self.calc_similarity(content_mat, self.content_sim_between_2users)
         start_sim_mat = self.calc_similarity(sleep_mat_start, hours_array_sim)
         end_sim_mat = self.calc_similarity(sleep_mat_end, hours_array_sim)
         # self.sleep_start_sim = self.calc_similarity(sleep_mat1, dtw)
-
-        self.similarity = self.content_sim * 0.4 + start_sim_mat * 0.3 + end_sim_mat * 0.3
-        print("similarity:")
-        print(self.similarity)
+        self.similarity = content_sim * Util.content_met_weight + start_sim_mat * Util.hours_met_weight + end_sim_mat * Util.hours_met_weight
 
     def predict_given_start_time(self, start_time, user):
         recommended_end_time = np.array([])
@@ -238,7 +227,8 @@ class Recommender:
         # highest_sim = self.similarity[index][highest_sim_index]
         sum_hour = 0
         for ind in highest_sim_index:
-            sum_hour += recommend_wake_time(start_time, get_sleep_of_user_from_db((self.users_list[highest_sim_index])[0]))
+            sum_hour += recommend_wake_time(start_time,
+                                            get_sleep_of_user_from_db((self.users_list[highest_sim_index])[0]))
         return sum_hour / self.recNum
 
     def predict_given_end_time(self, end_time, user):
--- Util.py
+++ Util.py
@@ -1,11 +1,23 @@
 import mysql.connector
 
+num_min_in_day = 1440
+min_in_8_hours = 480
+min_in_2_hours = 120
+min_in_1_hour = 60
+days_in_5_years = 1825
+height_sim = 0.2
+weight_sim = 15
+content_sim_weight = 0.25
+content_met_weight = 0.4
+hours_met_weight = 0.3
+per_hour_sim_factor = 1.2
+
 
 def connect_to_db():
     return mysql.connector.connect(
         host="localhost",
-        user="artiom",
-        password="password",
+        user="root",
+        password="223333",
         database="smart_sleeper"
     )
 
